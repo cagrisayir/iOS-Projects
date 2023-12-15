e3255b07b611e92c1409a523dde2d6fbb08a309a
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-15 11:12:48 +0300

    temprature-converter init

---
+++ temprature-converter/temprature-converter/temprature_converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  temprature_converterApp.swift
+//  temprature-converter
+//
+//  Created by Omer Cagri Sayir on 15.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct temprature_converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
