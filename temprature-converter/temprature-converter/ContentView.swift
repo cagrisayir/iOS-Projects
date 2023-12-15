6130123ebe9236276fd6e096da3bf38ee7f03d05
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-15 11:41:29 +0300

    - added header - added tempreture input

--- temprature-converter/temprature-converter/ContentView.swift
+++ temprature-converter/temprature-converter/ContentView.swift
@@ -8,12 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var stringDegree: String = ""
+    @State var degree: Int = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment:.leading) {
+            
+            HStack{
+                Spacer()
+                Text("Temperature converter")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                Spacer()
+            }
+            .padding(.bottom)
+            
+            HStack {
+                VStack(alignment:.leading) {
+                    Text("Degrees")
+                        .font(.headline)
+                        .fontWeight(.light)
+                    
+                    TextField("", text: $stringDegree)
+                        .padding(.vertical, 5)
+                        .overlay(Divider(), alignment: .bottom)
+                        .frame(width: 150)
+                    
+                }
+                VStack(alignment: .leading) {
+                    Text("Type")
+                        .font(.headline)
+                        .fontWeight(.light)
+                    
+                }
+            }
+            
+            Spacer()
         }
         .padding()
     }
